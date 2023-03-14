16d7523ddbe2b8a696cf6447c1e3b118fc1247ee
Sukomal07 <hypersukomal@gmail.com>
2023-03-14 20:33:59 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,10 @@
+function validateLinkedInURL(url) {
+    const regex = /^https:\/\/www\.linkedin\.com\/in\/[a-zA-Z0-9_-]{5,30}[a-zA-Z0-9]$/;
+    if (regex.test(url)) {
+        console.log(`${url} is a valid LinkedIn profile URL.`);
+    } else {
+        console.log(`${url} is not a valid LinkedIn profile URL.`);
+    }
+}
+
+validateLinkedInURL("https://www.linkedin.com/in/john-doe123")
